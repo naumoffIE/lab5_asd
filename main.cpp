b0150762f1c30ea0a4bdfa2cca20183312ab1384
naumoffIE <asfer364@gmail.com>
2025-05-13 11:36:46 +0400

    почти финальная версия (переписанный код)

--- main.cpp
+++ main.cpp
@@ -1,78 +1,90 @@
 #include <iostream>
 #include <cstdlib>
-#include <cstring>
-#include <ctime>
 #include <string>
+#include <ctime>
+#include <vector>
 
 using namespace std;
 
-template <typename K, typename T>
-class hash_table {
+template <typename Key, typename Value>
+class HashTable {
 private:
     struct Node {
-        K key;
-        T value;
+        Key key;
+        Value value;
         Node* next;
 
-        Node(const K& key_, const T& value_) : key(key_), value(value_), next(nullptr) { };
+        Node(const Key& k, const Value& v) : key(k), value(v), next(nullptr) {}
     };
 
     Node** table;
     int capacity;
     int size;
+    static constexpr double LOAD_FACTOR_THRESHOLD = 0.75;
 
-    int hash(K key) const {
+    int hash(const Key& key) const {
         return key % capacity;
     }
 
     void rehash() {
-        int old_capacity = capacity;
+        int oldCapacity = capacity;
+        Node** oldTable = table;
+
         capacity *= 2;
-        Node** new_table = new Node * [capacity];
-        for (int i = 0; i < old_capacity; i++)
-        {
-            for (Node* current = table[i]; current; current = current->next)
-            {
-                new_table.insert(current);
+        size = 0;
+        table = new Node * [capacity]();
+
+        for (int i = 0; i < capacity; ++i) {
+            table[i] = nullptr;
+        }
+
+        for (int i = 0; i < oldCapacity; ++i) {
+            Node* current = oldTable[i];
+            while (current) {
+                insert(current->key, current->value);
+                Node* temp = current;
+                current = current->next;
+                delete temp;
             }
         }
+
+        delete[] oldTable;
     }
+
 
 
 public:
-
-    hash_table() : size(0), capacity(0), table(nullptr) { }
-    // Конструктор по размеру
-    hash_table(int initial_capacity) {
-        capacity = initial_capacity;
-        size = 0;
-        table = new Node* [capacity];
-        for (int i = 0; i < capacity; ++i)
+    HashTable() : size(0), capacity(16), table(new Node* [16]()) {
+        for (int i = 0; i < capacity; ++i) {
             table[i] = nullptr;
+        }
     }
 
-    // Конструктор со случайными значениями
-    hash_table(int initial_capacity, int count) {
-        capacity = initial_capacity;
-        size = 0;
-        table = new Node* [capacity];
-        for (int i = 0; i < capacity; ++i)
+    HashTable(int initialCapacity) : size(0), capacity(initialCapacity) {
+        table = new Node * [capacity]();
+        for (int i = 0; i < capacity; ++i) {
             table[i] = nullptr;
+        }
+    }
 
-        srand((unsigned)time(0));
+    HashTable(int initialCapacity, int count) : size(0), capacity(initialCapacity) {
+        table = new Node * [capacity]();
+        for (int i = 0; i < capacity; ++i) {
+            table[i] = nullptr;
+        }
+
+        srand(static_cast<unsigned>(time(0)));
         for (int i = 0; i < count; ++i) {
-            K key = rand() % 1000;
-            T value = rand() % 100;
+            Key key = rand() % 1000;
+            Value value = rand() % 100;
             insert(key, value);
         }
     }
 
-    // Конструктор копирования
-    hash_table(const hash_table& other) {
-        capacity = other.capacity;
-        size = 0;
-        table = new Node * [capacity];
-        for (int i = 0; i < capacity; ++i){
+    HashTable(const HashTable& other) : size(0), capacity(other.capacity) {
+        table = new Node * [capacity]();
+        for (int i = 0; i < capacity; ++i) {
+            table[i] = nullptr;
             Node* current = other.table[i];
             while (current) {
                 insert(current->key, current->value);
@@ -81,33 +93,30 @@ public:
         }
     }
 
-    // Деструктор
-    ~hash_table() {
-            for (int i = 0; i < capacity; ++i) {
-                Node* current = table[i];
-                while (current) {
-                    Node* next = current->next;
-                    delete current;
-                    current = next;
-                }
-                table[i] = nullptr;
+    ~HashTable() {
+        for (int i = 0; i < capacity; ++i) {
+            Node* current = table[i];
+            while (current) {
+                Node* next = current->next;
+                delete current;
+                current = next;
             }
+            table[i] = nullptr;
+        }
+        size = 0;
         delete[] table;
     }
 
-    // Оператор присваивания
-    hash_table& operator=(const hash_table& other) {
+    HashTable& operator=(const HashTable& other) {
         if (this != &other) {
-            clear_table();
+            clearTable();
             delete[] table;
 
             capacity = other.capacity;
             size = 0;
-            table = new Node* [capacity];
-            for (int i = 0; i < capacity; ++i)
-                table[i] = nullptr;
-
+            table = new Node * [capacity]();
             for (int i = 0; i < capacity; ++i) {
+                table[i] = nullptr;
                 Node* current = other.table[i];
                 while (current) {
                     insert(current->key, current->value);
@@ -118,25 +127,32 @@ public:
         return *this;
     }
 
-    // Вставка
-    bool insert(K key, const T& value) {
+    bool insert(const Key& key, const Value& value) {
+        if ((size + 1) / capacity > LOAD_FACTOR_THRESHOLD) {
+            rehash();
+        }
+
         int index = hash(key);
         Node* current = table[index];
         while (current) {
-            if (current->key == key)
+            if (current->key == key) {
                 return false;
+            }
             current = current->next;
         }
-        Node* new_node = new Node( key, value );
-        Node* head = table[index];
-        new_node->next = head;
-        table[index] = new_node;
+
+        Node* newNode = new Node(key, value);
+        newNode->next = table[index];
+        table[index] = newNode;
         ++size;
         return true;
     }
 
-    // Вставка или обновление
-    void insert_or_assign(K key, T& value) {
+    void insertOrAssign(const Key& key, const Value& value) {
+        if ((size + 1) / capacity > LOAD_FACTOR_THRESHOLD) {
+            rehash();
+        }
+
         int index = hash(key);
         Node* current = table[index];
         while (current) {
@@ -146,22 +162,25 @@ public:
             }
             current = current->next;
         }
-        Node* new_node = new Node{ key, value, table[index] };
-        table[index] = new_node;
+
+        Node* newNode = new Node(key, value);
+        newNode->next = table[index];
+        table[index] = newNode;
         ++size;
     }
 
-    // Удаление
-    bool erase(K key) {
+    bool erase(const Key& key) {
         int index = hash(key);
         Node* current = table[index];
         Node* prev = nullptr;
         while (current) {
             if (current->key == key) {
-                if (prev)
+                if (prev) {
                     prev->next = current->next;
-                else
+                }
+                else {
                     table[index] = current->next;
+                }
                 delete current;
                 --size;
                 return true;
@@ -172,136 +191,77 @@ public:
         return false;
     }
 
-    // Поиск
-    T* search(K key) {
+    Value* search(const Key& key) {
         int index = hash(key);
         Node* current = table[index];
         while (current) {
-            if (current->key == key)
+            if (current->key == key) {
                 return &current->value;
+            }
             current = current->next;
         }
         return nullptr;
     }
 
-    // Проверка наличия значения
-    bool contains(T& value) const {
+    bool contains(const Value& value) const {
         for (int i = 0; i < capacity; ++i) {
             Node* current = table[i];
             while (current) {
-                if (current->value == value)
+                if (current->value == value) {
                     return true;
+                }
                 current = current->next;
             }
         }
         return false;
     }
 
-    // Количество элементов по хешу
-    int count(K key) {
-        int index = hash(key);
-        int cnt = 0;
-        Node* current = table[index];
-        while (current) {
-            ++cnt;
-            current = current->next;
+    int count(const Key& key) const {
+        int index = hash(key);              
+        int count = 0;                      
+        Node* current = table[index];       
+        while (current) {                  
+            ++count;                        
+            current = current->next;        
         }
-        return cnt;
+        return count;                       
     }
 
-    // Печать таблицы
     void print() const {
-        for (size_t i = 0; i < size; ++i) {
-            cout << "Bucket #" << i << ": ";
-            for (Node* node = table[i]; node != nullptr; node = node->next) {
-                cout << "[" << node->key << ", " << node->value << "] ";
+        for (int i = 0; i < capacity; ++i) {
+            if (table[i]) {
+                cout << "Bucket #" << i << ": ";
+                for (Node* node = table[i]; node; node = node->next) {
+                    cout << "[" << node->key << ", " << node->value << "] ";
+                }
+                cout << '\n';
             }
-            cout << '\n';
         }
     }
+
+
+  
 };
 
-
 int main() {
-    /*HashTable<int, int> map;
-    map.insert(1, 85);
-    map.insert(11, 86);
-    map.insert(2, 87);
-    map.insert(12, 88);
-
-    cout << "HashTable:\n";
-    map.print();
-
-    HashTable<int, int> map1(map);
-
-    cout << "\nCopied HashTable:\n";
-    map1.print();
-
-    map1.insert(3, 89);
-    map1.insert(33, 90);
-
-    cout << "\nCopied HashTable:\n";
-    map1.print();
-
-    map = map1;
-    cout << "\nHashTable:\n";
-    map.print();
-
-    map.insert_or_assign(23, 1);
-    map.insert_or_assign(33, 10);
-
-    cout << "\nHashTable:\n";
-    map.print();
-
-    map.insert_or_assign(23, 1000);
-
-    cout << "\nHashTable:\n";
-    map.print();
-
-    cout << "Is the table contains 85:" << map.contains(85) << "\n";
-
-    cout << "Value by index 23:" << *map.search(23);
-
-    map.erase(3);
-    map.insert(43, 12);
-    map.insert(53, 13);
-    map.insert(63, 14);
-
-    cout << "\nHashTable:\n";
-    map.print();
-    cout << map.count(63);*/
-
-
-
-    hash_table<char, int> romanNumbers;
-    romanNumbers.insert('I', 1);
-    romanNumbers.insert('V', 5);
-    romanNumbers.insert('X', 10);
-    romanNumbers.insert('L', 50);
-    romanNumbers.insert('C', 100);
-    romanNumbers.insert('D', 500);
-    romanNumbers.insert('M', 1000);
-    romanNumbers.print();
-
-    string input;
-    cout << "Enter romanial number ";
-    cin >> input;
-
-
-    int total = 0;
-    int prevValue = 0;
-
-    for (int i = input.length() - 1; i >= 0; i--) {
-        int currentValue = *romanNumbers.search(input[i]);
-        if (currentValue < prevValue) {
-            total -= currentValue;
+    vector<int> num{ 25,75,125,175,225,275,325,375,425,475 };
+    vector<float> collisions;
+    for (int i = 0; i < num.size(); i++)
+    {
+        size_t table_size = num[i];
+        float total_collisions = 0;
+        for (int i = 0; i < 100; i++)
+        {
+            HashTable<int, int> hash_table1(table_size, 25);
+            total_collisions += hash_table1.count_hashtable();
         }
-        else {
-            total += currentValue;
-        }
-        prevValue = currentValue;
+        total_collisions = total_collisions / 100;
+        collisions.push_back(total_collisions);
     }
-    cout << total;
-
+    for (int i = 0; i < collisions.size(); i++)
+    {
+        cout << collisions[i] << '\n';
+    }
+    return 0;
     return 0;
 }
