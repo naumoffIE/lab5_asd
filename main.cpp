c315a51931e7a3d05c4b0e6cadfd8c9f1df53578
naumoffIE <asfer364@gmail.com>
2025-05-13 14:01:59 +0400

    версия до правок Шапиро

--- main.cpp
+++ main.cpp
@@ -72,7 +72,6 @@ public:
         for (int i = 0; i < capacity; ++i) {
             table[i] = nullptr;
         }
-
         srand(static_cast<unsigned>(time(0)));
         for (int i = 0; i < count; ++i) {
             Key key = rand() % 1000;
@@ -83,6 +82,9 @@ public:
 
     HashTable(const HashTable& other) : size(0), capacity(other.capacity) {
         table = new Node * [capacity]();
+        for (int i = 0; i < capacity; ++i) {
+            table[i] = nullptr;
+        }
         for (int i = 0; i < capacity; ++i) {
             table[i] = nullptr;
             Node* current = other.table[i];
@@ -109,8 +111,6 @@ public:
 
     HashTable& operator=(const HashTable& other) {
         if (this != &other) {
-            clearTable();
-            delete[] table;
 
             capacity = other.capacity;
             size = 0;
@@ -128,19 +128,20 @@ public:
     }
 
     bool insert(const Key& key, const Value& value) {
-        if ((size + 1) / capacity > LOAD_FACTOR_THRESHOLD) {
-            rehash();
-        }
+        //if ((double)(size + 1) / capacity > LOAD_FACTOR_THRESHOLD) {
+        //    rehash();
+        //}
 
         int index = hash(key);
         Node* current = table[index];
-        while (current) {
-            if (current->key == key) {
-                return false;
-            }
-            current = current->next;
+        
+            while (current) {
+                if (current->key == key) {
+                    return false;
+                }
+                current = current->next;
+            
         }
-
         Node* newNode = new Node(key, value);
         newNode->next = table[index];
         table[index] = newNode;
@@ -149,9 +150,9 @@ public:
     }
 
     void insertOrAssign(const Key& key, const Value& value) {
-        if ((size + 1) / capacity > LOAD_FACTOR_THRESHOLD) {
-            rehash();
-        }
+        //if ((double)(size + 1) / capacity > LOAD_FACTOR_THRESHOLD) {
+        //    rehash();
+        //}
 
         int index = hash(key);
         Node* current = table[index];
@@ -239,29 +240,132 @@ public:
         }
     }
 
+    int count_collisions() const {
+        int counter = 0;
+        for (int i = 0; i < size; i++)
+        {
+            if (table[i]) {
+                Node* current = table[i];
+                int cur_count = count(current->key);
+                if (cur_count > 1)    counter += cur_count - 1;
+            }
+
+        }
+        return counter;
+    }
+
 
   
 };
 
 int main() {
-    vector<int> num{ 25,75,125,175,225,275,325,375,425,475 };
+    HashTable<int, int> hash_table1(3, 2);
+    vector<int> num{ 25,30,35,40,45,50,55,60,75,125,175,225,275,325,375,425,475 };
     vector<float> collisions;
     for (int i = 0; i < num.size(); i++)
     {
         size_t table_size = num[i];
-        float total_collisions = 0;
-        for (int i = 0; i < 100; i++)
+        float sum_median_collisions = 0;
+        for (int j = 0; j < 100; j++)
         {
             HashTable<int, int> hash_table1(table_size, 25);
-            total_collisions += hash_table1.count_hashtable();
+           /* hash_table1.print();*/
+            float total_collisions = hash_table1.count_collisions();
+            sum_median_collisions += (total_collisions / num[i]) * 100;
         }
-        total_collisions = total_collisions / 100;
-        collisions.push_back(total_collisions);
+        sum_median_collisions /= 100;
+      /*  total_collisions = total_collisions / 100;*/
+
+        collisions.push_back(sum_median_collisions);
     }
     for (int i = 0; i < collisions.size(); i++)
     {
         cout << collisions[i] << '\n';
     }
     return 0;
-    return 0;
 }
+
+//int main() {
+//    HashTable<int, int> map;
+//    map.insert(1, 85);
+//    map.insert(11, 86);
+//    map.insert(2, 87);
+//    map.insert(12, 88);
+//
+//    cout << "HashTable:\n";
+//    map.print();
+//
+//    HashTable<int, int> map1(map);
+//
+//    cout << "\nCopied HashTable:\n";
+//    map1.print();
+//
+//    map1.insert(3, 89);
+//    map1.insert(33, 90);
+//
+//    cout << "\nCopied HashTable:\n";
+//    map1.print();
+//
+//    map = map1;
+//    cout << "\nHashTable:\n";
+//    map.print();
+//
+//    map.insertOrAssign(23, 1);
+//    map.insertOrAssign(33, 10);
+//
+//    cout << "\nHashTable:\n";
+//    map.print();
+//
+//    map.insertOrAssign(23, 1000);
+//
+//    cout << "\nHashTable:\n";
+//    map.print();
+//
+//    cout << "Is the table contains 85:" << map.contains(85) << "\n";
+//
+//    cout << "Value by index 23:" << *map.search(23);
+//
+//    map.erase(3);
+//    map.insert(43, 12);
+//    map.insert(53, 13);
+//    map.insert(63, 14);
+//
+//    cout << "\nHashTable:\n";
+//    map.print();
+//    cout << map.count(63);
+//
+//
+//
+//    HashTable<char, int> romanNumbers;
+//    romanNumbers.insert('I', 1);
+//    romanNumbers.insert('V', 5);
+//    romanNumbers.insert('X', 10);
+//    romanNumbers.insert('L', 50);
+//    romanNumbers.insert('C', 100);
+//    romanNumbers.insert('D', 500);
+//    romanNumbers.insert('M', 1000);
+//    romanNumbers.print();
+//
+//    string input;
+//    cout << "Enter romanial number ";
+//    cin >> input;
+//
+//
+//    int total = 0;
+//    int prevValue = 0;
+//
+//    for (int i = input.length() - 1; i >= 0; i--) {
+//        int currentValue = *romanNumbers.search(input[i]);
+//        if (currentValue < prevValue) {
+//            total -= currentValue;
+//        }
+//        else {
+//            total += currentValue;
+//        }
+//        prevValue = currentValue;
+//    }
+//    cout << total;
+//
+//    return 0;
+//}
+
